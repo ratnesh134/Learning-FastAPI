de42dd906baceaa3beb900c65c0ca4a8f63ac515
Ratnesh Kumar <ratnesh134@gmail.com>
2025-07-29 22:12:29 +0530

    Main file update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import schema
 
 app = FastAPI()
 
@@ -8,4 +10,11 @@ async def root():
 
 @app.get("/posts")
 def get_post():
-    return {"data" : "This is your post"}
+    return {"data" : "This is your post"} 
+
+@app.post("/createposts")
+def create_post(payLoad: dict=Body(...)):
+
+    print(payLoad)
+
+    return {"new_post" : f"title : {payLoad['Title']} , Content : {payLoad['Content']}"}
